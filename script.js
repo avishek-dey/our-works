c827443a0e39587cb22d8b9171988c3122d70902
Avishek Dey <139633713+avishek-dey@users.noreply.github.com>
2024-02-12 19:18:17 +0000

    Pending changes exported from your codespace

--- script.js
+++ script.js
@@ -44,3 +44,51 @@ document.addEventListener("DOMContentLoaded", function () {
     columnToggleButton.classList.toggle("active"); // Toggle active class for button
   });
 });
+
+function handleEncryptedLinkClick(event) {
+  event.preventDefault(); // Prevent the default link behavior
+
+  var password = prompt("Please enter the password:");
+  if (password === "alohomora") {
+    // Replace "12345" with your actual password
+    window.location.href = "https://www.google.com"; // Redirect to Google after successful password entry
+  } else {
+    alert("Incorrect password. Access denied.");
+  }
+}
+
+Shery.makeMagnet(".btn" /* Element to target.*/, {
+  //Parameters are optional.
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  duration: 0.3,
+});
+
+Shery.imageMasker("img" /* Element to target.*/, {
+  //Parameters are optional.
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  duration: 0.3,
+});
+
+Shery.textAnimate(".h1" /* Element to target.*/, {
+  //Parameters are optional.
+  style: 1,
+  y: 10,
+  delay: 0.1,
+  duration: 0.6,
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  multiplier: 0.1,
+});
+
+// Function to fetch and inject HTML content
+function injectHTML(url, targetId) {
+  fetch(url)
+    .then((response) => response.text())
+    .then((html) => {
+      document.getElementById(targetId).innerHTML = html;
+    });
+}
+
+// Call the function to inject HTML content for header, content, and footer
+injectHTML("graphics.html", "header");
+injectHTML("lp.html", "content");
+injectHTML("mobile.html", "footer");
